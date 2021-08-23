2bb8583a8127cfd8f25e65a7b3b1ea9f2bc6dcb6
Adam Pearson <adam.pearson94@gmail.com>
2021-08-23 15:17:47 +0100

    Finished styling contact page, form now posts to db
    Remaining on this page:
    
    - Add error & success messages
    - Add JavaScript form validation
    - Add JavaScript to show/hide hidden text
    - Add JavaScript to show/hide map?

--- contact.php
+++ contact.php
@@ -17,38 +17,181 @@ include "./inc/nav.php";
 <div id="sidebar-overlay"></div> 
 <div id="main" class="pushed2">
 
+<div class="page-head">
+    <h1>Our Offices</h1>
+</div>
+<?php 
 
+// echo $_POST['name'] . "\n";
+// echo $_POST['email'] . "\n";
+// echo $_POST['phone_number'] . "\n";
+// echo $_POST['subject'] . "\n";
+// echo $_POST['message'] . "\n\n";
+
+// if (isset($_POST['newsletter_signup'])) {
+//     $newsletter = 1;
+// } else {
+//     $newsletter = 0;
+// }
+
+// var_dump($newsletter);
+
+
+if (isset($_POST['submit'])) {
+
+    if (isset($_POST['name'])
+    && isset($_POST['email'])
+    && isset($_POST['phone_number'])
+    && isset($_POST['subject'])
+    && isset($_POST['message'])) {
+       $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+       if (filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
+           $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+       } else {
+           echo "<p>Please enter a valid email address</p>";
+       }
+       $phone = filter_input(INPUT_POST, 'phone_number', FILTER_SANITIZE_STRING);
+       $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
+       $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+
+       if (isset($_POST['newsletter_signup'])) {
+            $newsletter = true;
+       } else {
+            $newsletter = false;
+       }
+
+   } else {
+       echo "<p>Please fill in all details</p>";
+   }
+
+   $contactArray = [
+    "name" => $name,
+    "email" => $email,
+    "phone" => $phone,
+    "subject" => $subject,
+    "message" => $message,
+    "newsletter" => $newsletter
+   ];
+
+   postContact($db, $contactArray);
+}
+
+ 
+
+?>
+<div class="contact-offices width-margin">    
+    <div class="offices-flex">
+        <div class="offices office-cam">
+            <div class="offices-block">
+                <div class="image">
+                    <a href="#">
+                        <img src="/img/cambridge.jpg" alt="Cambridge Office" class="office-img">
+                    </a>
+                </div>
+                <div class="content">
+                <a href="#"><h2>Cambridge Office</h2></a>
+                    <p>
+                        Unit 1.28, <br>
+                        St John's Innovation Centre, <br>
+                        Cowley Road, Milton, <br>
+                        Cambridge, <br>
+                        CB4 0WS
+                    </p>
+                    <a href="tel:01223375772" class="office-tel">01223 37 57 72</a>
+                    <button class="btn office-btn">
+                        View More
+                    </button>
+                </div>
+            </div>
+            <div class="offices-map">
+                <div class="mapouter"><div class="gmap_canvas"><iframe id="gmap_canvas" src="https://maps.google.com/maps?q=netmatters%20cambridge&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><br></div></div>
+            </div>
+        </div>
+        <div class="offices office-wym">
+            <div class="offices-block">
+                <div class="image">
+                    <a href="#">
+                        <img src="/img/wymondham.jpg" alt="Wymondham Office" class="office-img">
+                    </a>
+                </div>
+                <div class="content">
+                    <a href="#"><h2>Wymondham Office</h2></a>
+                    <p>
+                        Unit 15,<br>
+                        Penfold Drive,<br>
+                        Gateway 11 Business Park,<br>
+                        Wymondham, Norfolk,<br>
+                        NR18 0WZ
+                    </p>
+                    <a href="tel:01603704020" class="office-tel">01603 70 40 20</a>
+                    <button class="btn office-btn">
+                        View More
+                    </button>
+                </div>
+            </div>
+            <div class="offices-map">
+                <div class="mapouter"><div class="gmap_canvas"><iframe id="gmap_canvas" src="https://maps.google.com/maps?q=netmatters%20wymondham&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><br></div></div>
+            </div>
+        </div>
+        <div class="offices office-yar">
+            <div class="offices-block">
+                <div class="image">
+                    <a href="#">
+                        <img src="/img/yarmouth.jpg" alt="Great Yarmouth Office" class="office-img">
+                    </a>
+                </div>
+                <div class="content">
+                    <a href="#"><h2>Great Yarmouth Office</h2></a>
+                    <p>
+                        Suite F23,<br>
+                        Beacon Innovation Centre,<br>
+                        Beacon Park, Gorleston,<br>
+                        Great Yarmouth, Norfolk,<br>
+                        NR31 7RA
+                    </p>
+                    <a href="tel:01493603204" class="office-tel">01493 60 32 04</a>
+                    <button class="btn office-btn">
+                        View More
+                    </button>
+                </div>
+            </div>
+            <div class="offices-map">
+                <div class="mapouter"><div class="gmap_canvas"><iframe id="gmap_canvas" src="https://maps.google.com/maps?q=netmatters%20great%20yarmouth&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><br></div></div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="contact-outer width-margin">
     <div class="contact-inner">
+    <?php echo "<p>Error: Please fill out all details</p>"?>
     <form action="/contact.php" method="post">
-        <div class="form-group">
-            <label for="name" class="required"><strong>Your Name</strong></label>
+        <div class="contact-form-group">
+            <label for="name" class="required">Your Name</label>
             <input type="text" name="name" id="name" class="contact-field">
         </div>
-        <div class="form-group">
-            <label for="email" class="required"><strong>Your Email</strong></label>
+        <div class="contact-form-group">
+            <label for="email" class="required">Your Email</label>
             <input type="email" name="email" id="email" class="contact-field">
         </div>
 
-        <div class="form-group">
-            <label for="phone_number" class="required"><strong>Your Telephone Number</strong></label>
-            <input type="text" name="phone number" id="phone_number" class="contact-field">
+        <div class="contact-form-group">
+            <label for="phone_number" class="required">Your Telephone Number</label>
+            <input type="text" name="phone_number" id="phone_number" class="contact-field">
         </div>
-        <div class="form-group">
-            <label for="subject" class="required"><strong>Subject</strong></label>
+        <div class="contact-form-group">
+            <label for="subject" class="required">Subject</label>
             <input type="text" name="subject" id="subject" class="contact-field">
         </div>
-        <div class="form-group form-group-textarea">
-            <label for="message" class="required"><strong>Message</strong></label>
+        <div class="contact-form-group form-group-textarea">
+            <label for="message" class="required">Message</label>
             <textarea name="message"></textarea>
         </div>
-        <div class="marketing-check">
+        <div class="contact-marketing-check">
                 <div class="marketing-check-content">
                     <label class="checkbox-container">
-                        <input type="checkbox">
+                        <input type="checkbox" name="newsletter_signup[]" id="newsletter_signup">
                         <span class="checkmark checkmark-contact">
-                            <!-- <i class="fas fa-check"></i> -->
                             <span class="check material-icons-outlined">&#xe876</span>
                         </span>
                         <span class="checkbox-text">
@@ -57,17 +200,19 @@ include "./inc/nav.php";
                     </label>
                 </div>
             </div>
-        <input type="submit" class="btn contact-form-submit" value="SEND ENQUIRY"></input>
+        
+        <input type="submit" class="btn contact-form-submit" name="submit" value="SEND ENQUIRY"></input>
+        
     </form>
     </div>
 
     <div class="contact-side">
-        <p><strong>Email us on:</strong></p>
+        <p>Email us on:</p>
         <p><a href="mail:sales@netmatters.com" class="contact-email-text">sales@netmatters.com</a></p>
-        <p><strong>Business hours:</strong></p>
-        <p><strong>Monday - Friday 07:00 - 18:00</strong></p>
+        <p>Business hours:</p>
+        <p>Monday - Friday 07:00 - 18:00</p>
         <a href="#" class="out-of-hours-btn">
-            <p><strong>Out of Hours IT Support</strong></p>
+            <p>Out of Hours IT Support</p>
             <i class="fas fa-chevron-down"></i>
         </a>
         <div class="hidden-contact-text">
--- inc/functions.php
+++ inc/functions.php
@@ -1,5 +1,12 @@
 <?php
 
+// ini_set('display_errors', 0);
+
+/**
+ *  Pull news posts from the database
+ *  provide $db PDO object
+ *  limits to 3 results
+ */
 function pullNews($db) {
     try {
         $query = "SELECT news.heading, news.description, news.news_image, news.date, authors.full_name, authors.profile_image, category.category_name
@@ -12,6 +19,29 @@ function pullNews($db) {
         $stmt->execute();
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $results;
+    } catch (Exception $e) {
+        echo "Unable to connect - ";
+        echo $e->getMessage();
+    }
+}
+
+
+/**
+ *  Post contact form data to db
+ */
+
+function postContact($db, $contactArray) {
+    try {
+        $query = "INSERT INTO contact (email, name, phone_number, subject, message, newsletter_check)
+                 VALUES (:email, :name, :phone_number, :subject, :message, :newsletter_check)";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(":email", $contactArray['email']);
+        $stmt->bindParam(":name", $contactArray['name']);
+        $stmt->bindParam(":phone_number", $contactArray['phone']);
+        $stmt->bindParam(":subject", $contactArray['subject']);
+        $stmt->bindParam(":message", $contactArray['message']);
+        $stmt->bindParam(":newsletter_check", $contactArray['newsletter']);
+        $stmt->execute();
     } catch (Exception $e) {
         echo "Unable to connect - ";
         echo $e->getMessage();
